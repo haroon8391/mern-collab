cdd6b8bc14142f1cf441b498cc3d8c4af984d639
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-07-24 16:39:15 +0500

    Get jobs data from database

--- api/src/controllers/jobs.controller.ts
+++ api/src/controllers/jobs.controller.ts
@@ -1,35 +1,8 @@
 import { Request, Response } from "express";
 import Job from "../models/Job.model";
 
-const jobs = [
-	{
-		id: 1,
-		title: "Job One",
-		description: "This is the first job",
-	},
-	{
-		id: 2,
-		title: "Job Two",
-		description: "This is the second job",
-	},
-	{
-		id: 3,
-		title: "Job Three",
-		description: "This is the third job",
-	},
-	{
-		id: 4,
-		title: "Job Four",
-		description: "This is the fourth job",
-	},
-	{
-		id: 5,
-		title: "Job Five",
-		description: "This is the fifth job",
-	},
-];
-
-export const getJobs = (req: Request, res: Response) => {
+export const getJobs = async (req: Request, res: Response) => {
+	const jobs = await Job.find();
 	res.json(jobs);
 };
 
