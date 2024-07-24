cc667147a75b820009af169864465db33300a0f7
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-07-24 13:39:12 +0530

    Add Temp jobs

--- api/src/controllers/jobs.controller.ts
+++ api/src/controllers/jobs.controller.ts
@@ -1,7 +1,35 @@
-import express, { Request, Response } from "express";
+import { Request, Response } from "express";
+
+const jobs = [
+	{
+		id: 1,
+		title: "Job One",
+		description: "This is the first job",
+	},
+	{
+		id: 2,
+		title: "Job Two",
+		description: "This is the second job",
+	},
+	{
+		id: 3,
+		title: "Job Three",
+		description: "This is the third job",
+	},
+	{
+		id: 4,
+		title: "Job Four",
+		description: "This is the fourth job",
+	},
+	{
+		id: 5,
+		title: "Job Five",
+		description: "This is the fifth job",
+	},
+];
 
 export const getJobs = (req: Request, res: Response) => {
-	res.send("GET /jobs");
+	res.json(jobs);
 };
 
 export const getJob = (req: Request, res: Response) => {
