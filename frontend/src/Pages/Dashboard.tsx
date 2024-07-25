5fb6164e1f4117b241065ea62b688d568aa15242
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-07-25 16:27:27 +0530

    Add Loading Animation

--- frontend/src/Pages/Dashboard.tsx
+++ frontend/src/Pages/Dashboard.tsx
@@ -59,7 +59,28 @@ const Dashboard: React.FC = () => {
 				Job Dashboard
 			</h1>
 			{loading ? (
-				<p>Loading...</p>
+				<div className="flex justify-center items-center min-h-[50vh]">
+					<svg
+						className="animate-spin h-5 w-5 text-gray-800"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+					>
+						<circle
+							className="opacity-25"
+							cx="12"
+							cy="12"
+							r="10"
+							stroke="currentColor"
+							strokeWidth="4"
+						></circle>
+						<path
+							className="opacity-75"
+							fill="currentColor"
+							d="M4 12a8 8 0 018-8v8H4z"
+						></path>
+					</svg>
+				</div>
 			) : (
 				<div className="space-y-4">
 					{error && <p className="text-red-500">{error}</p>}
