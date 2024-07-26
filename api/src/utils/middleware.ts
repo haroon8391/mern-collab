5c1f485fc6a0763214c177590d6311f4b46d0343
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-07-26 12:25:06 +0500

    Add Error Handlers

--- api/src/utils/middleware.ts
+++ api/src/utils/middleware.ts
@@ -24,6 +24,14 @@ const errorHandler = (
 		return res.status(400).send({ error: "malformatted id" });
 	} else if (error.name === "ValidationError") {
 		return res.status(400).json({ error: error.message });
+	} else if (error.name === "JsonWebTokenError") {
+		return res.status(401).json({ error: "invalid token", msg: error.message });
+	} else if (error.name === "TokenExpiredError") {
+		return res.status(401).json({ error: "token expired", msg: error.message });
+	} else if (error.name === "MongoServerError") {
+		return res.status(400).json({ error: "duplicate key", msg: error.message });
+	} else if (error.name === "TypeError") {
+		return res.status(400).json({ error: "type error", msg: error.message });
 	}
 
 	next(error);
