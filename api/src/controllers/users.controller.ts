5ee17f2ea63fa17ae58ccb64012d76e56015cf9f
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-07-26 15:13:47 +0500

    Complete CRUD Operations API

--- api/src/controllers/users.controller.ts
+++ api/src/controllers/users.controller.ts
@@ -1,28 +1,74 @@
 import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import User from "../models/User.model";
 
 // GET /api/v1/users
-const getUsers = (req: Request, res: Response) => {
-	res.send("GET /users");
+const getUsers = async (req: Request, res: Response) => {
+	const users = await User.find({});
+	res.json(users);
 };
 
 // GET /api/v1/users/:id
-const getUser = (req: Request, res: Response) => {
-	res.send("GET /users/:id");
+const getUser = async (req: Request, res: Response) => {
+	const { id } = req.params;
+
+	const user = await User.findById(id);
+	if (!user) {
+		return res.status(404).json({ message: "User not found" });
+	}
+
+	res.json(user);
 };
 
 // POST /api/v1/users/:id
-const createUser = (req: Request, res: Response) => {
-	res.send("POST /users");
+const createUser = async (req: Request, res: Response) => {
+	const { username, email, password } = req.body;
+
+	// Hash password
+	const passwordHash = await bcrypt.hash(password, 10);
+
+	// Create new user
+	const newUser = new User({ username, email, password: passwordHash });
+
+	// Save user to database
+	const savedUser = await newUser.save();
+	res.json(savedUser);
 };
 
 // PUT /api/v1/users/:id
-const updateUser = (req: Request, res: Response) => {
-	res.send("PUT /users/:id");
+const updateUser = async (req: Request, res: Response) => {
+	const { id } = req.params;
+
+	const { username, email, password } = req.body;
+
+	const updateObj = {} as any;
+
+	if (username) updateObj["username"] = username;
+	if (email) updateObj["email"] = email;
+	if (password) {
+		const passwordHash = await bcrypt.hash(password, 10);
+		updateObj["password"] = passwordHash;
+	}
+
+	const user = await User.findByIdAndUpdate(id, updateObj, { new: true });
+	if (!user) {
+		return res.status(404).json({ message: "User not found" });
+	}
+
+	res.json(user);
 };
 
 // DELETE /api/v1/users/:id
-const deleteUser = (req: Request, res: Response) => {
-	res.send("DELETE /users/:id");
+const deleteUser = async (req: Request, res: Response) => {
+	const { id } = req.params;
+
+	const user = await User.findByIdAndDelete(id);
+
+	if (!user) {
+		return res.status(404).json({ message: "User not found" });
+	}
+
+	res.json(user);
 };
 
 export default {
