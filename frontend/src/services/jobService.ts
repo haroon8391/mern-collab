332ca07f93ba46b239551eb6fcc3d685d667b902
Muhammad Haroon Ali <haroon8391@gmail.com>
2024-07-26 13:53:53 +0500

    update changes

--- frontend/src/services/jobService.ts
+++ frontend/src/services/jobService.ts
@@ -3,28 +3,28 @@ import JobType from "../types/JobTypes";
 const baseUrl = "/api/v1/jobs";
 
 const getAll = async () => {
-	const response = await axios.get(baseUrl);
-	return response.data;
+  const response = await axios.get(baseUrl);
+  return response.data;
 };
 
 const create = async (credentials: JobType) => {
-	const response = await axios.post(baseUrl, credentials);
-	return response.data;
+  const response = await axios.post(baseUrl, credentials);
+  return response.data;
 };
 
 const update = async (id: string, jobToUpdate: JobType) => {
-	const response = await axios.put(`${baseUrl}/${id}`, jobToUpdate);
-	return response.data;
+  const response = await axios.put(`${baseUrl}/${id}`, jobToUpdate);
+  return response.data;
 };
 
 const remove = async (id: string) => {
-	const response = await axios.delete(`${baseUrl}/${id}`);
-	return response.data;
+  const response = await axios.delete(`${baseUrl}/${id}`);
+  return response.data;
 };
 
 export default {
-	getAll,
-	create,
-	update,
-	remove,
+  getAll,
+  create,
+  update,
+  remove,
 };
