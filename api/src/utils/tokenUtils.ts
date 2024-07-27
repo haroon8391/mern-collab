1e4f274e41ca5ced9977e3e3ca21ba8213180852
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-07-27 18:15:56 +0530

    Resolve token issue

--- api/src/utils/tokenUtils.ts
+++ api/src/utils/tokenUtils.ts
@@ -1,21 +1,19 @@
 import jwt, { JwtPayload } from "jsonwebtoken";
-import dotenv from "dotenv";
+import config from "./config";
 
-dotenv.config();
-
-const secretKey = process.env.JWT_SECRET_KEY || "collab123";
+const secretKey = config.JWT_SECRET || "collab123";
 
 interface VerifyTokenResult {
-  valid: boolean;
-  decoded?: JwtPayload | string;
-  error?: string;
+	valid: boolean;
+	decoded?: JwtPayload | string;
+	error?: string;
 }
 
 export function verifyToken(token: string): VerifyTokenResult {
-  try {
-    const decoded = jwt.verify(token, secretKey);
-    return { valid: true, decoded };
-  } catch (error) {
-    return { valid: false, error: (error as Error).message };
-  }
+	try {
+		const decoded = jwt.verify(token, secretKey);
+		return { valid: true, decoded };
+	} catch (error) {
+		return { valid: false, error: (error as Error).message };
+	}
 }
