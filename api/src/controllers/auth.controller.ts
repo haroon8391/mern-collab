ac97aebe1c16765ffebd0eed3f60d48eb50684ae
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-07-30 15:23:58 +0500

    Refactor User Model

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -1,9 +1,6 @@
 import { Request, Response } from "express";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
 
 import User from "../models/User.model";
-import config from "../utils/config";
 
 // POST /api/v1/auth/login
 const login = async (req: Request, res: Response) => {
@@ -13,48 +10,31 @@ const login = async (req: Request, res: Response) => {
 		return res.status(400).json({ error: "Email and password are required" });
 	}
 
-	const user = await User.findOne({ email });
-	if (!user) {
+	const userInDb = await User.findOne({ email });
+	if (!userInDb) {
 		return res.status(401).json({ error: "Invalid email or password" });
 	}
 
-	const isPasswordCorrect = await bcrypt.compare(password, user.password);
+	const isPasswordCorrect = await userInDb.comparePassword(password);
 	if (!isPasswordCorrect) {
 		return res.status(401).json({ error: "Invalid email or password" });
 	}
 
-	const userForToken = {
-		name: user.name,
-		id: user._id,
-	};
-
-	const token = jwt.sign(userForToken, config.JWT_SECRET, {
-		expiresIn: "1h",
-	});
-
+	const token = userInDb.createJWT();
 	res.status(200).send({
+		name: userInDb.name,
 		token,
-		name: user.name,
 	});
 };
 
 // POST /api/v1/auth/register
 const register = async (req: Request, res: Response) => {
-	const { name, email, password, role } = req.body;
+	const newUserInstance = new User({ ...req.body });
 
-	const saltRounds = 10;
-	const passwordHash = await bcrypt.hash(password, saltRounds);
+	const userCreatedInDb = await newUserInstance.save();
+	const token = userCreatedInDb.createJWT();
 
-	const user = new User({
-		name,
-		email,
-		password: passwordHash,
-		role,
-	});
-
-	await user.save();
-
-	res.status(201).json({ message: "User created", user });
+	res.status(201).json({ user: { name: userCreatedInDb.name }, token });
 };
 
 export default {
--- api/src/models/User.model.ts
+++ api/src/models/User.model.ts
@@ -1,6 +1,19 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import config from "../utils/config";
 
-const userSchema = new mongoose.Schema({
+interface IUser extends Document {
+	name: string;
+	email: string;
+	password: string;
+	role: "user" | "admin";
+	jobs: mongoose.Schema.Types.ObjectId[];
+	comparePassword(password: string): Promise<boolean>;
+	createJWT(): string;
+}
+
+const userSchema: Schema<IUser> = new mongoose.Schema({
 	name: {
 		type: String,
 		required: [true, "Username is required"],
@@ -39,6 +52,23 @@ userSchema.set("toJSON", {
 		delete returnedObject.__v;
 	},
 });
+
+userSchema.pre("save", async function () {
+	const salt = await bcryptjs.genSalt(10);
+	this.password = await bcryptjs.hash(this.password, salt);
+});
+
+userSchema.methods.comparePassword = async function (password: string) {
+	return await bcryptjs.compare(password, this.password);
+};
+
+userSchema.methods.createJWT = function () {
+	const payload = { userId: this._id, role: this.role };
+	const token = jwt.sign(payload, process.env.JWT_SECRET as string, {
+		expiresIn: "1d",
+	});
+	return token;
+};
 
 const User = mongoose.model("User", userSchema);
 
