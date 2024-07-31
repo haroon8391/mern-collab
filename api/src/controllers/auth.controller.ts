72e5cec3e0860e0a07eccf5206bc858135a373a0
Muhammad Haroon Ali <haroon8391@gmail.com>
2024-07-31 15:42:33 +0500

    updated admin functionality in the navbar

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -6,44 +6,44 @@ import config from "../utils/config";
 
 // POST /api/v1/auth/login
 const login = async (req: Request, res: Response) => {
-	const { email, password } = req.body;
+  const { email, password } = req.body;
 
-	if (!email || !password) {
-		return res.status(400).json({ error: "Email and password are required" });
-	}
+  if (!email || !password) {
+    return res.status(400).json({ error: "Email and password are required" });
+  }
 
-	const userInDb = await User.findOne({ email });
-	if (!userInDb) {
-		return res.status(401).json({ error: "Invalid email or password" });
-	}
+  const userInDb = await User.findOne({ email });
+  if (!userInDb) {
+    return res.status(401).json({ error: "Invalid email or password" });
+  }
 
-	const isPasswordCorrect = await userInDb.comparePassword(password);
+  const isPasswordCorrect = await userInDb.comparePassword(password);
 
-	if (!isPasswordCorrect) {
-		return res.status(401).json({ error: "Invalid email or password" });
-	}
+  if (!isPasswordCorrect) {
+    return res.status(401).json({ error: "Invalid email or password" });
+  }
 
-	const token = userInDb.createJWT();
+  const token = userInDb.createJWT();
 
-	res.status(200).send({
-		token,
-		name: userInDb.name,
-		userId: userInDb._id,
-		role: userInDb.admin,
-	});
+  res.status(200).send({
+    token,
+    name: userInDb.name,
+    userId: userInDb._id,
+    isAdmin: userInDb.admin,
+  });
 };
 
 // POST /api/v1/auth/register
 const register = async (req: Request, res: Response) => {
-	const user = new User(req.body);
+  const user = new User(req.body);
 
-	const userInDb = await user.save();
-	const token = userInDb.createJWT();
+  const userInDb = await user.save();
+  const token = userInDb.createJWT();
 
-	res.status(201).json({ user: { name: user.name }, token });
+  res.status(201).json({ user: { name: user.name }, token });
 };
 
 export default {
-	login,
-	register,
+  login,
+  register,
 };
--- frontend/src/store/authSlice.ts
+++ frontend/src/store/authSlice.ts
@@ -1,32 +1,39 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const getDataFromLocalStorage = (key: string) => {
-	const data = localStorage.getItem(key);
-	return data ? JSON.parse(data) : null;
+  const data = localStorage.getItem(key);
+  return data ? JSON.parse(data) : null;
 };
 
 const initialState = {
-	isAuthenticated: getDataFromLocalStorage("isAuthenticated") || false,
-	userData: getDataFromLocalStorage("userData") || null,
+  isAuthenticated: getDataFromLocalStorage("isAuthenticated") || false,
+  userData: getDataFromLocalStorage("userData") || null,
+  isAdmin: getDataFromLocalStorage("userData")
+    ? getDataFromLocalStorage("userData").isAdmin
+    : false,
 };
 
 export const authSlice = createSlice({
-	name: "auth",
-	initialState,
-	reducers: {
-		login: (state, action) => {
-			state.isAuthenticated = true;
-			state.userData = action.payload;
-			localStorage.setItem("isAuthenticated", "true");
-			localStorage.setItem("userData", JSON.stringify(action.payload));
-		},
-		logout: (state) => {
-			state.isAuthenticated = false;
-			state.userData = null;
-			localStorage.setItem("isAuthenticated", "false");
-			localStorage.setItem("userData", JSON.stringify(null));
-		},
-	},
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.isAuthenticated = true;
+      state.userData = action.payload;
+      state.isAdmin = action.payload.isAdmin;
+      localStorage.setItem("isAuthenticated", "true");
+      localStorage.setItem("userData", JSON.stringify(action.payload));
+      localStorage.setItem("isAdmin", JSON.stringify(action.payload.isAdmin));
+    },
+    logout: (state) => {
+      state.isAuthenticated = false;
+      state.userData = null;
+      state.isAdmin = false;
+      localStorage.setItem("isAuthenticated", "false");
+      localStorage.setItem("userData", JSON.stringify(null));
+      localStorage.setItem("isAdmin", JSON.stringify(false));
+    },
+  },
 });
 
 export const { login, logout } = authSlice.actions;
