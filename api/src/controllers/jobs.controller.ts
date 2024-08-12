84291f9f13da1592effc09257e238566cb7f172c
Muhammad Haroon Ali <haroon8391@gmail.com>
2024-08-12 22:36:04 +0500

    updated create job page

--- api/src/controllers/jobs.controller.ts
+++ api/src/controllers/jobs.controller.ts
@@ -3,84 +3,86 @@ import Job from "../models/Job.model";
 import User from "../models/User.model";
 
 export const getJobs = async (req: Request, res: Response) => {
-	const { createdBy } = req.query;
+  const { createdBy } = req.query;
 
-	const queryObject = {
-		...(createdBy && { createdBy }),
-	};
+  const queryObject = {
+    ...(createdBy && { createdBy }),
+  };
 
-	const jobs = await Job.find(queryObject);
-	res.json(jobs);
+  const jobs = await Job.find(queryObject);
+  res.json(jobs);
 };
 
 export const getJob = async (req: Request, res: Response) => {
-	const { id } = req.params;
+  const { id } = req.params;
 
-	const job = await Job.findById(id);
+  const job = await Job.findById(id);
 
-	if (!job) {
-		return res.status(404).json({ message: "Job not found" });
-	}
+  if (!job) {
+    return res.status(404).json({ message: "Job not found" });
+  }
 
-	res.json(job);
+  res.json(job);
 };
 
 export const createJob = async (req: Request, res: Response) => {
-	req.body.createdBy = req.user?.userId;
-	const job = new Job(req.body);
-	const newJob = await job.save();
-	res.json(newJob);
+  console.log(req.user);
+
+  req.body.createdBy = req.body.createdBy || req.user?.userId;
+  const job = new Job(req.body);
+  const newJob = await job.save();
+  res.json(newJob);
 };
 
 export const updateJob = async (req: Request, res: Response) => {
-	const { id } = req.params;
-	const body = req.body;
+  const { id } = req.params;
+  const body = req.body;
 
-	const userInDb = await User.findById(req.user?.userId);
-	if (!userInDb) {
-		return res.status(404).json({ message: "User not found" });
-	}
+  const userInDb = await User.findById(req.user?.userId);
+  if (!userInDb) {
+    return res.status(404).json({ message: "User not found" });
+  }
 
-	const jobInDb = await Job.findById(id);
-	if (!jobInDb) {
-		return res.status(404).json({ message: "Job not found" });
-	}
+  const jobInDb = await Job.findById(id);
+  if (!jobInDb) {
+    return res.status(404).json({ message: "Job not found" });
+  }
 
-	if (jobInDb.createdBy.toString() !== req.user?.userId) {
-		return res.status(401).json({ message: "Unauthorized" });
-	}
+  if (jobInDb.createdBy.toString() !== req.user?.userId) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
 
-	const updatedJob = await Job.findByIdAndUpdate(id, body, { new: true });
+  const updatedJob = await Job.findByIdAndUpdate(id, body, { new: true });
 
-	if (!updatedJob) {
-		return res.status(404).json({ message: "Job not found" });
-	}
+  if (!updatedJob) {
+    return res.status(404).json({ message: "Job not found" });
+  }
 
-	res.json(updatedJob);
+  res.json(updatedJob);
 };
 
 export const deleteJob = async (req: Request, res: Response) => {
-	const { id } = req.params;
+  const { id } = req.params;
 
-	const userInDb = await User.findById(req.user?.userId);
-	if (!userInDb) {
-		return res.status(404).json({ message: "User not found" });
-	}
+  const userInDb = await User.findById(req.user?.userId);
+  if (!userInDb) {
+    return res.status(404).json({ message: "User not found" });
+  }
 
-	const jobInDb = await Job.findById(id);
-	if (!jobInDb) {
-		return res.status(404).json({ message: "Job not found" });
-	}
+  const jobInDb = await Job.findById(id);
+  if (!jobInDb) {
+    return res.status(404).json({ message: "Job not found" });
+  }
 
-	if (jobInDb.createdBy.toString() !== req.user?.userId) {
-		return res.status(401).json({ message: "Unauthorized" });
-	}
+  if (jobInDb.createdBy.toString() !== req.user?.userId) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
 
-	const job = await Job.findByIdAndDelete(id);
+  const job = await Job.findByIdAndDelete(id);
 
-	if (!job) {
-		return res.status(404).json({ message: "Job not found" });
-	}
+  if (!job) {
+    return res.status(404).json({ message: "Job not found" });
+  }
 
-	res.json(job);
+  res.json(job);
 };
